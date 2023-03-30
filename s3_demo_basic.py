dc5497cb25563673f50a8d9366c2e90c60c10e5d
Bradley Leonard <bradley@leonard.pub>
2023-03-30 18:07:55 -0400

    style:  cleaned up code
    cleaned up code for both presentability and to describe to the demoer
    what is happening in the different sections of the demo.
    additionally, added support for the demoer to specify their own file
    to upload as part of the demo.

--- s3_demo_basic.py
+++ s3_demo_basic.py
@@ -2,6 +2,7 @@
 
 import argparse
 import configparser
+import os
 import sys
 import time
 
@@ -27,6 +28,12 @@ def parse_args(args):
                         help="Alternate configuration file",
                         required=False,
                         default="")
+    parser.add_argument("-p", "--pause",
+                        help="Pause during the demo instead of waiting for "
+                        "60 seconds",
+                        required=False,
+                        action="store_true",
+                        default="")
     return parser.parse_args(args)
 
 
@@ -39,6 +46,8 @@ def list_buckets(clientS3):
         print('List of existing buckets:')
         for bucket in response['Buckets']:
             print(f'  {bucket["Name"]}')
+
+        print("\n")
         return "success"
     except ClientError as e:
         return e.response["Error"]
@@ -58,11 +67,11 @@ def list_bucket_contents(clientS3, bucket: str):
         objects = clientS3.list_objects_v2(Bucket=bucket)  # noqa
         fileCount = objects['KeyCount']
         if fileCount == 0:
-            print('bucket', bucket, 'is empty.')
+            print('\nBucket', bucket, 'is empty.')
         else:
-            print('bucket', bucket, 'has ', fileCount, ' objects.')
+            print('\nBucket', bucket, 'has ', fileCount, ' objects.')
             for obj in objects['Contents']:
-                print(' object name: ', obj['Key'])
+                print('  object name: ', obj['Key'])
         return "success"
     except ClientError as e:
         return e.response["Error"]
@@ -81,12 +90,15 @@ def delete_bucket(clientS3, bucket: str):
         objects = clientS3.list_objects_v2(Bucket=bucket)  # noqa
         fileCount = objects['KeyCount']
         if fileCount == 0:
-            print('bucket', bucket, 'is empty.')
+            print('Bucket', bucket, 'is empty.')
         else:
-            print('bucket', bucket, 'has ', fileCount, ' objects.')
+            print('Bucket', bucket, 'has ', fileCount, ' objects.')
             for obj in objects['Contents']:
-                print(' deleting object: ', obj['Key'])
+                print('  Deleting object: ', obj['Key'])
                 delete_file(clientS3, bucket, obj['Key'])
+
+            print("\n")
+
         print('Deleting bucket: ', bucket)
         response = clientS3.delete_bucket(Bucket=bucket)  # noqa
         return response
@@ -114,12 +126,14 @@ def main():
     secret_access_key = config['s3_demo_basic']['secret_access_key']
     accesser = config['s3_demo_basic']['accesser_url']
     bucket = config['s3_demo_basic']['bucket']
+    filename = config['s3_demo_basic']['file']
 
     print("\ns3_demo_basic Configuration:")
     print("         Access Key: ", access_key)
     print("  Secret Access Key: ", secret_access_key)
     print("       Accesser URL: ", accesser)
     print("             Bucket: ", bucket)
+    print("           Filename: ", filename)
     print("")
 
     #
@@ -132,38 +146,64 @@ def main():
                       aws_secret_access_key=secret_access_key,
                       endpoint_url=accesser)
 
+    print("\nThere are three (3) parts to the demo.\n")
+    print("\nPART 1:  This part of the demo will create a bucket.")
+    print("It will list existing buckets, create the bucket, and list "
+          "the existing buckets again.\n")
+
     # retrieve the list of existing buckets
     list_buckets(clientS3)
 
     # create bucket
     print("Creating bucket:     ", bucket)
     create_bucket(clientS3, bucket)
+    time.sleep(1)
+    print("")
 
     # Retrieve the list of existing buckets
     list_buckets(clientS3)
 
+    if argument.pause:
+        input("\nPress enter to continue...")
+    else:
+        print("\nWaiting for 60 seconds for you to check the GUI for the "
+              "existence of the bucket.\n")
+        time.sleep(60)
+
+    print("\nPART 2:  This part of the demo will upload a file to the newly "
+          "created bucket.")
+    print("It will list the contents of the bucket, upload an object, and "
+          "then list the contents of the bucket again.\n")
+
     # list contents of bucket
     list_bucket_contents(clientS3, bucket)
+    print("")
 
     # upload files
     print("Uploading file to bucket:     ", bucket)
-    with open('/usr/bin/dockerd', 'rb') as data:
-        clientS3.upload_fileobj(data, bucket, 'dockerd')
+    with open(filename, 'rb') as data:
+        clientS3.upload_fileobj(data, bucket, os.path.basename(filename))
 
     # list contents of bucket
     list_bucket_contents(clientS3, bucket)
 
+    if argument.pause:
+        input("\nPress enter to continue...")
+    else:
+        print("\nWaiting for 60 seconds for you to check the bucket for "
+              "for the existence of the newly uploaded object.\n")
+        time.sleep(60)
+
+    print("\nPART 3:  This part of the demo will delete a bucket.")
+    print("It will list existing buckets, list the contents of the bucket, "
+          "delete the bucket, and list the existing buckets again.\n")
+
     # Retrieve the list of existing buckets
     list_buckets(clientS3)
 
-    print('sleep 60 seconds, waiting for you to check out the bucket')
-    time.sleep(60)
-
     # delete bucket
     delete_bucket(clientS3, bucket)
-
-    print('sleep 60 seconds, waiting for bucket to be deleted')
-    time.sleep(60)
+    print("")
 
     # Retrieve the list of existing buckets
     list_buckets(clientS3)
